fff37a29de983ef92ac988f97a760a0c923ba42e
Omar Abdelrahman <132823099+OmarAbdelrahaman@users.noreply.github.com>
2023-11-02 17:45:09 +0200

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -11,11 +11,17 @@ public class Parser {
         commandName = "";
         args = new String[10];
     }
-    public void parse(String input) {
+    public boolean parse(String input) {
         String[] words = input.split(" ");
+        if (words.length > 0) {
         commandName = words[0];
         if (words.length > 1) {
             args = Arrays.copyOfRange(words, 1, words.length);
+        }
+        return true;
+        }
+        else {
+            return false;
         }
     }
 
